96d51ab38e572affea43783102e1bd8961ba9ba5
javaprogrammers35 <0987bvg@gmail.com>
2026-03-14 20:51:25 +0200

    mini-fix(bug fixes)

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -5,7 +5,7 @@ public class Utils {
 
     public static final int CELL_SIZE = 30;
 
-    public static final int[] SHIP_SIZES = {1,1,1,1,2,2,3,3,4,5};
+    public static final int[] SHIP_SIZES = {1,1,1,1,2,2,2,3,3,4};
 
     public static boolean inBounds(int x, int y) {
         return x >= 0 && y >= 0 && x < BOARD_SIZE && y < BOARD_SIZE;
