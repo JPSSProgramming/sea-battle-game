c331aed8cd46e16bd2c0b9d44714ef7f290da415
javaprogrammers35 <0987bvg@gmail.com>
2026-08-17 11:29:40 +0300

    chore: update main file

--- src/main/java/org/sea/battle/game/Main.java
+++ src/main/java/org/sea/battle/game/Main.java
@@ -1,12 +1,11 @@
 package org.sea.battle.game;
 
-import org.sea.battle.game.view.MainMenu;
+import org.sea.battle.game.view.MainWindow;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainMenu::new);
-
+        SwingUtilities.invokeLater(MainWindow::new);
     }
 }
